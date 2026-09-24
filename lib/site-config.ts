3f99c80518c9515948f305dad07cff0c3ffc09cc
jovokh88 <jovokh93@gmail.com>
2026-09-24 06:13:19 +0500

    fix: unify public restaurant data

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,17 +1,16 @@
-export const siteConfig = {
-  address: "Qodirjon Imomov ko'chasi, Jizzax",
-  phone: "+998 95 503 33 33",
-  tel: "tel:+998955033333",
-  telegram: "https://t.me/starburger_uz",
-  telegramOrderBot: "https://t.me/starburgeruzbot",
-  instagram: "https://instagram.com/starburger_uz",
-  googleMaps: "https://www.google.com/maps/place/Star+Burger/data=!4m7!3m6!1s0x38b29535aaee394f:0x43f63058c923975f!8m2!3d40.110259!4d67.8467107!16s%2Fg%2F11f68h_gkm",
-  yandexMaps: "https://yandex.com/maps/org/star_burger/205284545149/",
-  googleReviews: "https://search.google.com/local/reviews?placeid=ChIJTznuqjWVsjgRX5cjyVgw9kM",
-  yandexReviews: "https://yandex.com/maps/org/star_burger/205284545149/reviews/",
-  mapSearch: "https://www.google.com/maps/search/?api=1&query=Star+Burger%2C+Qodirjon+Imomov+ko%27chasi%2C+Jizzax",
-  menuUrl: "https://starburger.qrcha.uz",
+export const siteConfig={
+siteUrl:"https://starburger-site.vercel.app",
+address:"Qodirjon Inomov ko'chasi, 73",
+phone:"+998 95 503 33 33",tel:"tel:+998955033333",
+telegram:"https://t.me/starburger_uz",telegramOrderBot:"https://t.me/starburgeruzbot",
+instagram:"https://instagram.com/starburger_uz",
+googleMaps:"https://www.google.com/maps/place/Star+Burger/data=!4m7!3m6!1s0x38b29535aaee394f:0x43f63058c923975f!8m2!3d40.110259!4d67.8467107!16s%2Fg%2F11f68h_gkm",
+yandexMaps:"https://yandex.com/maps/org/star_burger/205284545149/",
+googleReviews:"https://search.google.com/local/reviews?placeid=ChIJTznuqjWVsjgRX5cjyVgw9kM",
+yandexReviews:"https://yandex.com/maps/org/star_burger/205284545149/reviews/",
+mapSearch:"https://www.google.com/maps/search/?api=1&query=Star+Burger%2C+Qodirjon+Inomov+ko%27chasi+73%2C+Jizzax",
+menuUrl:"https://starburger.qrcha.uz",
+ogImage:"https://starburger-site.vercel.app/images/hero.jpg"
 } as const
-
-export type SiteConfig = typeof siteConfig
+export type SiteConfig=typeof siteConfig
 export default siteConfig
